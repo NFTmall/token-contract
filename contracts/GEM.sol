2e34f3c445f9ad43da78bff1062258bc2c2704b8
Zi Wei Zhang <ziweidream@gmail.com>
2021-04-12 15:24:36 -0400

    truffle setup

---
+++ contracts/GEM.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.0;
+
+import "./openzeppelin-contracts/access/Ownable.sol";
+import "./openzeppelin-contracts/security/Pausable.sol";
+import "./openzeppelin-contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "./openzeppelin-contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+
+// mock class using ERC20
+contract INf4mation is Ownable, Pausable, ERC20Snapshot {
+
+    uint256 private _cap;
+
+    constructor (
+        string memory name,
+        string memory symbol,
+        uint256 cap_
+    ) ERC20(name, symbol) {
+        _cap = cap_;
+    }
+
+    /**
+     * @dev Triggers stopped state.
+     * @dev This function can only be called by the owner of the contract.
+     *
+     * Requirements:
+     *
+     * - The contract must not be paused.
+     */
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Returns to normal state.
+     * @dev This function can only be called by the owner of the contract.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /**
+     * @dev Creates a new snapshot and returns its snapshot id.
+     *
+     * Emits a {Snapshot} event that contains the same id.
+     * Only the owner of this contract can create a snapshot.
+     */
+    function snapshot() external onlyOwner {
+        _snapshot();
+    }
+
+    /**
+     * @dev Mint `amount` tokens to `account`.
+     * @dev This function can only be called by the owner of the contract.
+     */
+    function mint(address account, uint256 amount) public onlyOwner returns (bool) {
+        require(totalSupply() + amount <= cap(), "INFormation: cap exceeded");
+        _mint(account, amount);
+        return true;
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     */
+
+    function burn(uint256 amount) public returns (bool) {
+        _burn(_msgSender(), amount);
+        return true;
+    }
+   
+    function cap() public view returns (uint256) {
+        return _cap;
+    }
+
+    /**
+     * @dev See {ERC20-_ and ERC20Snapshot-_ beforeTokenTransfer}.
+     *
+     * Requirements:
+     *
+     * - the contract must not be paused.
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+
+        require(!paused(), "ERC20Pausable: token transfer while paused");
+    }
+
+}
---
+++ truffle-config.js
@@ -0,0 +1,83 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv').config()
+
+const mnemonicPhrase = process.env.MNEMONICS;
+
+module.exports = {
+  compilers: {
+    solc: {
+      version: "0.8.0",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    mainnet: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: mnemonicPhrase
+          },
+          providerOrUrl: "https://mainnet.infura.io/v3/process.env.INFURA_KEY"
+        }),
+      network_id: '1',
+    },
+    development: {
+      host: "127.0.0.1",
+      port: 8544,
+      network_id: 999 // Match any network id
+    },
+    ropsten: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: mnemonicPhrase
+          },
+          providerOrUrl: "https://ropsten.infura.io/v3/process.env.INFURA_KEY"
+        }),
+      network_id: '3',
+    },
+    rinkeby: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: mnemonicPhrase
+          },
+          providerOrUrl: "https://rinkeby.infura.io/v3/process.env.INFURA_KEY"
+        }),
+      network_id: '4',
+    },
+    kovan: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: mnemonicPhrase
+          },
+          providerOrUrl: "https://kovan.infura.io/v3/process.env.INFURA_KEY"
+        }),
+      network_id: '42',
+    },
+    live: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 1 // Ethereum public network
+      // optional config values:
+      // gas
+      // gasPrice
+      // from - default address to use for any transaction Truffle makes during migrations
+      // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+      //          - function that returns a web3 provider instance (see below.)
+      //          - if specified, host and port are ignored.
+    },
+  },
+  mocha: {
+    enableTimeouts: false,
+    useColors: true
+  }
+};
