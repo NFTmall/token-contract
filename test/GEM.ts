2f581baf2b0595f1bedebc9daf91fbfab29a941e
Pavel Rubin <pavel@nftmall.io>
2021-04-23 22:55:36 +0300

    Add tests for unpause and snapshot

--- test/GEM.ts
+++ test/GEM.ts
@@ -3,6 +3,7 @@ import {
 } from "../types/truffle-contracts";
 
 const { BN, constants, expectEvent, expectRevert, time } = require("@openzeppelin/test-helpers");
+const { expect, should } = require('chai');
 
 const GEM = artifacts.require("GEM");
 
@@ -18,6 +19,8 @@ contract("GEM", ([deployer, daoMultisig, whitelistedUser, user1, user2, user3]) 
         token.transfer(whitelistedUser, web3.utils.toWei('1000', 'ether'), {from:daoMultisig});
         token.transfer(user1, web3.utils.toWei('1000', 'ether'), {from:daoMultisig});
     });
+
+    // === Pause and whitelist functionality testing ===
 
     it("should initially be not paused", async () => {   
         let paused = await token.paused();
@@ -37,6 +40,7 @@ contract("GEM", ([deployer, daoMultisig, whitelistedUser, user1, user2, user3]) 
             "transfers paused"
         );
     });
+
     it("should not allow arbitrary transferFrom when paused", async () => {   
         let amount = web3.utils.toWei('10', 'ether');
         await token.approve(user3, amount);
@@ -59,5 +63,55 @@ contract("GEM", ([deployer, daoMultisig, whitelistedUser, user1, user2, user3]) 
         expectEvent(res, "Transfer");
     });
 
+    it("can be unpaused", async () => {   
+        await token.unpause({from:daoMultisig});
+        let paused = await token.paused();
+        expect(paused).to.be.false;
+    });
+
+    it("should allow arbitrary transfer when not paused", async () => {   
+        let amount = web3.utils.toWei('10', 'ether');
+        let res = await token.transfer(user2, amount, {from:user1});
+        expectEvent(res, "Transfer");
+    });
+
+    // === snapshot testing ===
+    it("should not take a snapshot from arbitrary user", async () => {   
+        await expectRevert(
+            token.snapshot({from: user1}),
+            "!admin"
+        );
+    });
+
+    it("admin should take a snapshot", async () => {   
+        let res = await token.snapshot({from: daoMultisig});
+        expectEvent(res, "Snapshot");
+    });
+
+    it("should be able to return balance at snapshot", async () => {   
+        let before = {
+            user1balance: await token.balanceOf(user1),
+            user2balance: await token.balanceOf(user2),
+        }
+
+        let res = await token.snapshot({from: daoMultisig});
+        const snapshotId = res.logs[0].args[0];
+        expectEvent(res, "Snapshot", {id:snapshotId});
+
+        let amount = new BN(web3.utils.toWei('5', 'ether'));
+        await  token.transfer(user1, amount, {from:user2});
+
+        let after = {
+            user1balance: await token.balanceOf(user1),
+            user2balance: await token.balanceOf(user2),
+            user1balanceSnapshot: await token.balanceOfAt(user1, snapshotId),
+            user2balanceSnapshot: await token.balanceOfAt(user2, snapshotId),
+        }
+
+        expect(after.user1balance).to.be.bignumber.equal(before.user1balance.add(amount));
+        expect(after.user2balance).to.be.bignumber.equal(before.user2balance.sub(amount));
+        expect(after.user1balanceSnapshot).to.be.bignumber.equal(before.user1balance);
+        expect(after.user2balanceSnapshot).to.be.bignumber.equal(before.user2balance);
+    });
 
 });
