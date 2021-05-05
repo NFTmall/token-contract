7c723e43ba15a438dc689118dce0a530e9c74e7f
captainheart <captaindev90@gmail.com>
2021-05-05 08:31:13 +0000

    feat: remove token recovery methods

--- contracts/GEM.sol
+++ contracts/GEM.sol
@@ -74,36 +74,6 @@ contract GEM is AccessControl, ERC20, Pausable, ERC20Burnable, ERC20Snapshot, ER
     }
 
     /**
-     * @notice A safety measure to recover accidentally locked BNB(ETH)
-     */
-    function withdrawETH() external onlyAdmin {
-        uint256 balance = address(this).balance;
-        payable(_msgSender()).transfer(balance);
-    }
-
-    /**
-     * @notice A safety measure to recover accidentally locked BEP20(ERC20) tokens
-     */
-    function withdrawERC20(IERC20 token) external onlyAdmin {
-        uint256 balance = token.balanceOf(address(this));
-        token.safeTransfer(_msgSender(), balance);
-    }
-
-    /**
-     * @notice A safety measure to recover accidentally locked ERC721 NFT
-     */
-    function withdrawERC721(IERC721 token, uint256 id) external onlyAdmin {
-        token.transferFrom(address(this), _msgSender(), id);
-    }
-
-    /**
-     * @notice A safety measure to recover accidentally locked ERC1155 NFT
-     */
-    function withdrawERC1155(IERC1155 token, uint256 id, uint256 amount, bytes calldata data) external onlyAdmin {
-        token.safeTransferFrom(address(this), _msgSender(), id, amount, data);
-    }
-
-    /**
      * @dev This function is overridden in both ERC20 and ERC20Snapshot, so we need to specify execution order here.
      */
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Snapshot) {
